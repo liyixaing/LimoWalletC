ee8462ce87c7693f150772ea609a7e8556ab92cc
liyixaing <1693174121@qq.com>
2019-12-23 10:50:12 +0800

    come one

--- app/src/main/java/com/xms/limowallet/activity/ServerActivity.java
+++ app/src/main/java/com/xms/limowallet/activity/ServerActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -99,6 +100,7 @@ public class ServerActivity extends BaseActivity {
         } else {
             mRootUrl = null;
             mTvMessage.setText("未获取服务器ip地址");
+            Log.e("xaioqaing", "xiaoqaing");
         }
     }
 
